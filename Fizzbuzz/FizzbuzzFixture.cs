9594b7ed0e3a8f0dc49834d788253fe9422ff211
GinoGalotti <gino.galotti@gmail.com>
2019-05-23 16:43:18 +0200

    initial commit

---
+++ Fizzbuzz/FizzbuzzFixture.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using System;
+
+
+namespace FizzBuzz
+{
+    namespace FizzBuzz.Tests
+    {
+        [TestFixture]
+        public class FizzBuzzKataFixture
+        {
+            [Test]
+            public void TestExample()
+            {
+                Console.WriteLine("hi");
+            }
+        }
+    }
+
+}
