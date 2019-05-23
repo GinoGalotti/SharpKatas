fbd99fef185b58ba3a37220525eaaa6714175c32
GinoGalotti <gino.galotti@gmail.com>
2019-05-23 23:01:22 +0200

    Create boilerplate

---
+++ FizzBuzzKata/Fizzbuzz/Fizzbuzz.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Fizzbuzz
+{
+    public class Fizzbuzz
+    {
+        public Fizzbuzz()
+        {
+            Console.WriteLine("init");
+        }
+
+        public void DoStuff()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+
+    // Driver Class 
+    public class Program
+    {
+
+        // Main Method 
+        static public void Main()
+        {
+            Console.WriteLine("I'm here");
+        }
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Hello there
+
+In this repo I am planning to have some simple code Katas to freshen up my C# skills. The goal isn't to find the perfect solution, but to have a solution working in under an hour. 
+
+I will also try different approaches of creating C# solutions/projects
